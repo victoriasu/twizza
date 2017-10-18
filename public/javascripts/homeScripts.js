a6e4b7afc41f4d1599760a7ea902616396455cc8
victoriasu <vicky-su-@hotmail.com>
2017-10-18 11:46:30 +1000

    Added sentiment selection

--- public/javascripts/homeScripts.js
+++ public/javascripts/homeScripts.js
@@ -1,33 +1,23 @@
 const hostname = 'localhost';
 const port = 3000;
 
-window.onload = function() { 
-    //Get submit button
+window.onload = function() {
+    // Get search elements
     var searchTerm = document.getElementById("searchField");
-    /*
-    //Add listener to submit button
-    if(searchTerm.addEventListener) {
-        searchTerm.addEventListener("click", function() {
-            if (searchTerm.value == '') {
-                searchTerm.value = '';
-            }
-        });
-    }
-    */
-
     const form = document.querySelector('form');
     var button = document.getElementById('searchButton');
 
+    // URL to redirect page to
     function getSearchURL() {
         searchURL = 'http://' + hostname + ':' + port + '/search/' + searchTerm.value;
         console.log(searchURL);
         return searchURL;
     }
 
+    // Go to URL when button is clicked
     button.addEventListener('click', (e) => {
         e.preventDefault();
         form.action = getSearchURL();
         form.submit();
     })
-
 }
