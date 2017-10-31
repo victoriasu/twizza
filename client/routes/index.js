f593460e74f4d32fe07a2c91a041e04df1b692d7
victoriasu <vicky-su-@hotmail.com>
2017-10-31 12:47:16 +1000

    Tweets are taken from database. Most frequent words is added.

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,21 +1,129 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    var embeddedStr = '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">5-year-old: We don&#39;t have pizza enough.<br><br>Me: We had it three days in a row.<br><br>5: I know.</p>&mdash; James Breakwell (@XplodingUnicorn) <a href="https://twitter.com/XplodingUnicorn/status/922829170664792064?ref_src=twsrc%5Etfw">October 24, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">When’s the best time to eat pizza?</p>&mdash; Jack&#39;s Pizza (@JacksPizza) <a href="https://twitter.com/JacksPizza/status/922981776552857600?ref_src=twsrc%5Etfw">October 25, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Guys and gals which Pizza is the best???</p>&mdash; Ty🏄🏽 (@VillaTyler21) <a href="https://twitter.com/VillaTyler21/status/922978057299697666?ref_src=twsrc%5Etfw">October 25, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I, literally, love pizza! When ordering <a href="https://twitter.com/PapaJohnsHousTx?ref_src=twsrc%5Etfw">@PapaJohnsHousTx</a> on game days, enter code ALTUVE for a chance to meet me! <a href="https://t.co/AWjRMjVXkH">pic.twitter.com/AWjRMjVXkH</a></p>&mdash; Jose Altuve (@JoseAltuve27) <a href="https://twitter.com/JoseAltuve27/status/922649377411420161?ref_src=twsrc%5Etfw">October 24, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    res.render('index', { title: 'Twizza', twitterResults: embeddedStr });
+var config = require('./../../config.json');
+var mysql = require('mysql');
+var pizzaTweetsCon = mysql.createConnection({
+    host: 'pizzatweets.cfdpyrxr6oll.us-west-2.rds.amazonaws.com',
+    user: config.mysqluser,
+    password: config.mysqlpass,
+    database: 'pizzatweets'
 });
+
+router.get('/', function(req, res, next) {
+    // Display tweets
+    let str = '';
+    let words = null;
+    async.parallel([
+        function(callback) {
+            pizzaTweetsCon.query({
+                sql: "SELECT * from tweets",
+            }, function (error, results, fields) {
+                if (error) console.log(error);
+                if (error) throw error;
+                str = getEmbedString(results);
+                callback();
+            });
+        },
+        function(callback) {
+            // Display words
+            pizzaTweetsCon.query({
+                sql: "SELECT * from wordcount ORDER BY count DESC LIMIT 10",
+            }, function (error, results, fields) {
+                if (error) console.log(error);
+                if (error) throw error;
+                console.log(results);
+                words = results;
+                callback();
+            });
+        }
+    ], function(err) {
+        if (err) return next(err);
+        res.render('index', { title: 'Twizza', twitterResults: str, frequentWords: words });
+    });
+});
+
 
 router.get('/:sentiment', function(req, res, next) {
-    var embeddedStr = '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">5-year-old: We don&#39;t have pizza enough.<br><br>Me: We had it three days in a row.<br><br>5: I know.</p>&mdash; James Breakwell (@XplodingUnicorn) <a href="https://twitter.com/XplodingUnicorn/status/922829170664792064?ref_src=twsrc%5Etfw">October 24, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">When’s the best time to eat pizza?</p>&mdash; Jack&#39;s Pizza (@JacksPizza) <a href="https://twitter.com/JacksPizza/status/922981776552857600?ref_src=twsrc%5Etfw">October 25, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Guys and gals which Pizza is the best???</p>&mdash; Ty🏄🏽 (@VillaTyler21) <a href="https://twitter.com/VillaTyler21/status/922978057299697666?ref_src=twsrc%5Etfw">October 25, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    embeddedStr += '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I, literally, love pizza! When ordering <a href="https://twitter.com/PapaJohnsHousTx?ref_src=twsrc%5Etfw">@PapaJohnsHousTx</a> on game days, enter code ALTUVE for a chance to meet me! <a href="https://t.co/AWjRMjVXkH">pic.twitter.com/AWjRMjVXkH</a></p>&mdash; Jose Altuve (@JoseAltuve27) <a href="https://twitter.com/JoseAltuve27/status/922649377411420161?ref_src=twsrc%5Etfw">October 24, 2017</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
-    res.render('index', { title: 'Twizza', twitterResults: embeddedStr });
+    if (req.params.sentiment == 'positive') {
+        // Display tweets
+        let str = '';
+        let words = null;
+        async.parallel([
+            function(callback) {
+                pizzaTweetsCon.query({
+                    sql: "SELECT * from tweets WHERE `tweets`.`sent` >= 0",
+                }, function (error, results, fields) {
+                    if (error) console.log(error);
+                    if (error) throw error;
+                    str = getEmbedString(results);
+                    callback();
+                });
+            },
+            function(callback) {
+                // Display words
+                pizzaTweetsCon.query({
+                    sql: "SELECT * from wordcount ORDER BY count DESC LIMIT 10",
+                }, function (error, results, fields) {
+                    if (error) console.log(error);
+                    if (error) throw error;
+                    console.log(results);
+                    words = results;
+                    callback();
+                });
+            }
+        ], function(err) {
+            if (err) return next(err);
+            res.render('index', { title: 'Twizza', twitterResults: str, frequentWords: words });
+        });
+    }
+    else if (req.params.sentiment == 'negative') {
+        // Display tweets
+        let str = '';
+        let words = null;
+        async.parallel([
+            function(callback) {
+                pizzaTweetsCon.query({
+                    sql: "SELECT * from tweets WHERE `tweets`.`sent` < 0",
+                }, function (error, results, fields) {
+                    if (error) console.log(error);
+                    if (error) throw error;
+                    str = getEmbedString(results);
+                    callback();
+                });
+            },
+            function(callback) {
+                // Display words
+                pizzaTweetsCon.query({
+                    sql: "SELECT * from wordcount ORDER BY count DESC LIMIT 10",
+                }, function (error, results, fields) {
+                    if (error) console.log(error);
+                    if (error) throw error;
+                    console.log(results);
+                    words = results;
+                    callback();
+                });
+            }
+        ], function(err) {
+            if (err) return next(err);
+            res.render('index', { title: 'Twizza', twitterResults: str, frequentWords: words });
+        });
+    }
 });
+
+function getEmbedString (results) {
+    let str = '';
+    for (let i = 0; i < 100; i++) {
+        str += '<blockquote class="twitter-tweet" data-lang="en">'
+        + '<a href="'
+        + 'https://twitter.com/' + results[i].user 
+        + '/status/' + results[i].id + '?ref_src=twsrc%5Etfw'
+        + '"></a>'
+        + '</blockquote> '
+        + '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+    }
+    return str;
+}
 
 module.exports = router;
