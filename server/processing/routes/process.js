62ff18d59aa2985fa44700b0870168fc6734143e
victoriasu <vicky-su-@hotmail.com>
2017-11-01 11:15:45 +1000

    Only adds to wordcount database every 10 tweets

--- server/processing/routes/process.js
+++ server/processing/routes/process.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 
 var sentiment = require('sentiment');
 var twitterText = require('twitter-text');
 var natural = require('natural');
 var tokenizer = new natural.WordTokenizer();
-var nlp = require('compromise');
+//var nlp = require('compromise');
+
+var tweetCount = 0;
+var tweets = [];
+var words = {};
+var totalWords100 = 0;
 
 var config = require('./../../../config.json');
 var mysql = require('mysql');
@@ -24,15 +30,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.put('/:sentiment', function(req, res, next) {
-    console.log("Sentiment: " + req.params.sentiment);
     var tweet = req.body;
-    //console.log(tweet);
+    tweetCount++;
+    console.log('tweetCount = ' + tweetCount);
 
     // Clean up text (remove mentions, hashtags and links)
     tweet.clean_text = removeLinks (tweet.text);
+    //console.log('clean text: ' + tweet.clean_text);
+
     // Get sentiment of cleaned text
     tweet.sentiment_value = sentiment(tweet.clean_text).score;
-    console.log(tweet);
     
     // Store tweet in database (table: tweets)
     var post = {id: null, twitterid: tweet.id, user: tweet.user, sent: tweet.sentiment_value};
@@ -42,26 +49,98 @@ router.put('/:sentiment', function(req, res, next) {
     });
     console.log(query.sql);
     
-    // Store word count in database (table: wordcount)
-    var tokenized = tokenizer.tokenize(tweet.clean_text);
-    // Remove retweet indicator
-    if (tokenized[0] === 'RT') {
-        tokenized.splice(0, 1);
-    }
-    for (let i = 0; i < tokenized.length; i++) {
-        pizzaTweetsCon.query({
-            sql: "INSERT INTO `wordcount` (word, count) VALUES('" + tokenized[i] + "' ," + "1) ON DUPLICATE KEY UPDATE word = '" + tokenized[i] + "' , count = count + 1",
-        }, function (error, results, fields) {
+    tweets.push(tweet);
+
+    if (tweetCount % 10 === 0) {
+        console.log('tweetCount % 10 === 0');
+        console.log('There are ' + tweets.length + ' tweets');
+        async.series([
+            function(callback) {
+                totalWords100 = 0;
+                // Clear the previous 100 tweets' data from last100count table
+                pizzaTweetsCon.query({
+                    sql: "TRUNCATE TABLE last100count",
+                }, function (error, results, fields) {
+                    if (error) console.log(error);
+                    if (error) throw error;
+                    console.log('Table has been truncated');
+                    callback();
+                });
+            },
+            function(callback) {
+                console.log('Time to loop');
+                for (let t = 0; t < tweets.length; t++) {
+                    // Loop through words in this tweet and update words dictionary
+                    var tokenized = tokenizer.tokenize(tweets[t].clean_text);
+                    // Remove retweet indicator
+                    if (tokenized[0] === 'RT') {
+                        tokenized.splice(0, 1);
+                    }
+                    for (let j = 0; j < tokenized.length; j++) {
+                        totalWords100++;
+                        if (words.hasOwnProperty(tokenized[j])) {
+                            words[tokenized[j]][0]++;
+                        }
+                        else {
+                            words[tokenized[j]] = [1, 0];
+                        }
+                        // Update wordcount table with tokenised words and their count
+                        pizzaTweetsCon.query({
+                            sql: "INSERT INTO `wordcount` (word, count) VALUES('" + tokenized[j] + "' ," + "1) ON DUPLICATE KEY UPDATE word = '" + tokenized[j] + "' , count = count + 1",
+                        }, function (error, results, fields) {
+                            if (error) console.log(error);
+                            if (error) throw error;
+                        });
+                    }
+                    console.log('t = ' + t);
+                }
+                callback();
+            },
+            function(callback) {
+                console.log("Number of words: " + totalWords100);
+                for (var word in words) {
+                    async.series([
+                        function(callback) {
+                            // Calculate percentage of word occurrence in last 100 tweets
+                            words[word][1] = words[word][0] / totalWords100;
+                            console.log('percent = ' + words[word][0] + '/' + totalWords100 + ' = ' + words[word][1]);
+                            console.log('word: ' + word);
+                            callback();
+                        },
+                        function(callback) {
+                            console.log('hEEEELLLLOOOOO????');
+                            pizzaTweetsCon.query({
+                                sql: "INSERT INTO `last100count` (word, count, percent) VALUES('hellooo' ,1 ,0.567438)",
+                            }, function (error, results, fields) {
+                                if (error) console.log(error);
+                                if (error) throw error;
+                                console.log('insert into last100count');
+                                callback();
+                            });
+                            // Store this into database
+                            pizzaTweetsCon.query({
+                                sql: "INSERT INTO `last100count` (word, count, percent) VALUES('" + word + "' ," + words[word][0] + " ," + words[word][1] + ")",
+                            }, function (error, results, fields) {
+                                if (error) console.log(error);
+                                if (error) throw error;
+                                console.log('INSERTEDDDDDDDD into last100count');
+                                callback();
+                            });
+                        }
+                    ]);
+                }
+            }
+        ],
+        function(err, results) {
             if (error) console.log(error);
-            if (error) throw error;
+            console.log('async done');
         });
     }
-
     // Do NLP
-    console.log(tweet.clean_text);
-    let getPeopleAndCounts = nlp(tweet.clean_text).people().out('freq');
-    console.log(nlp(tweet.clean_text).sentences().toPastTense());
-    console.log(getPeopleAndCounts);
+    // console.log(tweet.clean_text);
+    // let getPeopleAndCounts = nlp(tweet.clean_text).people().out('freq');
+    // console.log(nlp(tweet.clean_text).sentences().toPastTense());
+    // console.log(getPeopleAndCounts);
     // End response or send success so that load balancer knows this instance is healthy
     res.end();
     // res.status(200);
