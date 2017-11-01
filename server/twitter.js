f60574f3975a42f675d87726fe257e34348bd4da
khuslen <khuslen96@yahoo.com>
2017-11-01 23:56:11 +1000

    removed another console.log

--- server/twitter.js
+++ server/twitter.js
@@ -12,21 +12,12 @@ t = new twitter({
     token_secret: config.twitter_access_token_secret
 })
 
-// DONE: Not getting to index.js router.put ('sentiment') section
-// Load balancer not connected to instances or instances not healthy
-// Had to open port 80 in security groups of load balancer
-//var url = 'https://pizza-tweets-854907392.us-west-2.elb.amazonaws.com:80/positive';
-//var url = 'http://pizza-tweets-854907392.us-west-2.elb.amazonaws.com/positive';
+var url = 'http://elb-cab432-1649828362.us-west-2.elb.amazonaws.com/positive';
 //var url = 'http://localhost:3000/positive';
-var url = 'http://54.213.228.143:3000/positive';
 
 var tweetToSend = {};
 
 t.on('tweet', function (tweet) {
-    //console.log('tweet received', tweet);
-    // console.log('Count: ' + count);
-    // count++;
-
     // Send the tweet text, user.screen_name, and id_str to the Load Balancer
     tweetToSend = {id: tweet.id_str, user: tweet.user.screen_name, text: tweet.text};
     var options = {
